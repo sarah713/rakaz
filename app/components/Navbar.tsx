e76c865f1d3fa9bb7b81d61006892a5b2473ea95
Sarah <sarahkulsoom18@gmail.com>
2023-11-06 23:38:24 +0530

    created components

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -22,6 +22,7 @@ const Navbar = () => {
             <div className="lg:flexCenter hidden">
                 <Button type="button" title="Login" icon='./user.svg' variant='btn_dark_green' />
             </div>
+            <Image src="menu.svg" alt="menu" width={32} height={32} className="inline-block cursor-pointer lg:hidden" />
         </nav>
     )
 }
